3aa070b9c06206d802554ed3b19dcd97161c266d
Arne Goedeke <el@laramies.com>
2018-04-21 10:48:05 +0200

    Controller: moved tcp nodejs code into seperate file

---
+++ src/controller/TCP.js
@@ -0,0 +1,42 @@
+import {
+    Socket
+  } from 'net';
+
+import {
+    ClientConnection
+  } from '../Controller';
+
+export class TCPConnection extends ClientConnection
+{
+  constructor(socket)
+  {
+    super();
+    this.socket = socket;
+    socket.on('data', (data) => {
+      this.read(data.buffer);
+    });
+  }
+
+  static connect(options)
+  {
+    return new Promise((resolve, reject) => {
+      const socket = new Socket();
+      const onerror = function(ev) {
+        reject(ev);
+      };
+      socket.on('error', onerror);
+      socket.on('timeout', onerror);
+      socket.on('connect', () => {
+        socket.removeListener('error', onerror);
+        socket.removeListener('timeout', onerror);
+        resolve(new this(socket));
+      });
+      socket.connect(options);
+    });
+  }
+
+  write(buf)
+  {
+    this.socket.write(Buffer.from(buf), 'binary');
+  }
+}
