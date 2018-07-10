391b61cee83505f790a62d53059c514324bf661b
Arne Goedeke <el@laramies.com>
2018-07-10 17:40:15 +0200

    WebSocketConnection: support nodejs

--- src/controller/WebSocket.js
+++ src/controller/WebSocket.js
@@ -6,14 +6,40 @@ export class WebSocketConnection extends ClientConnection
   {
     super();
     ws.binaryType = "arraybuffer";
-    ws.onmessage = (ev) => {
+    ws.addEventListener('message', (ev) => {
       this.read(ev.data);
-    };
+    });
     this.ws = ws;
   }
 
   write(buf)
   {
     this.ws.send(buf);
+  }
+
+  static connect(options)
+  {
+    return new Promise((resolve, reject) => {
+      let ws;
+      if (typeof WebSocket !== 'undefined')
+      {
+        ws = new WebSocket(options.url);
+      }
+      else
+      {
+        const WebSocket = require('ws');
+        ws = new WebSocket(options.url);
+      }
+
+      const on_error = function(e) {
+        reject(e);
+      };
+
+      ws.addEventListener('open', () => {
+        ws.removeEventListener('error', on_error);
+        resolve(new this(ws));
+      });
+      ws.addEventListener('error', on_error);
+    });
   }
 }
