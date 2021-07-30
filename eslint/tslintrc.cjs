bbb537ac98ec762c5496e250e8b036e00c1c6cf9
Arne Goedeke <arne@deuso.de>
2021-07-30 10:15:37 +0200

    [lint]: Add linter target for typescript

---
+++ eslint/tslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2017": true,
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+      "prefer-const" : "error",
+      "no-unused-vars" : [ "error", { "args": "none" } ],
+      "no-var" : "error"
+    }
+};
