fcc96b00d1ea933195db98674297071ece79e113
Arne Goedeke <arne@deuso.de>
2021-08-06 19:13:21 +0200

    OCP1: Add encodeMessageTo

--- src/OCP1/encode_message.js
+++ src/OCP1/encode_message.js
@@ -1,36 +1,57 @@
-export function encodeMessage(a) {
-  if (!Array.isArray(a)) a = [a];
+export const messageHeaderSize = 10;
 
-  // message header
-  let len = 10;
-  const type = a[0].messageType;
+function calculateMessageLength(pdus) {
+  let len = messageHeaderSize;
+  const type = pdus[0].messageType;
 
-  for (let i = 0; i < a.length; i++) {
-    const tmp = a[i];
-    if (tmp.messageType != type)
+  for (let i = 0; i < pdus.length; i++) {
+    const pdu = pdus[i];
+    if (pdu.messageType != type)
       throw new Error('Cannot combine different types in one message.');
-    len += tmp.encoded_length();
+    len += pdu.encoded_length();
   }
 
-  const buf = new ArrayBuffer(len);
-  const dst = new DataView(buf);
+  return len;
+}
 
-  let pos = 0;
+export function encodeMessageTo(dst, pos, pdus, offset, end) {
+  if (!offset)
+    offset = 0;
+
+  if (!end)
+    end = pdus.length;
 
   dst.setUint8(pos, 0x3b);
   pos += 1;
+
+  const startPos = pos;
+
   dst.setUint16(pos, 1);
   pos += 2;
-  dst.setUint32(pos, len - 1);
+  const lenPos = pos;
   pos += 4;
-  dst.setUint8(pos, type);
+  dst.setUint8(pos, pdus[offset].messageType);
   pos++;
-  dst.setUint16(pos, a.length);
+  dst.setUint16(pos, end - offset);
   pos += 2;
 
-  for (let i = 0; i < a.length; i++) {
-    pos = a[i].encode_to(dst, pos);
+  for (let i = offset; i < end; i++) {
+    pos = pdus[i].encode_to(dst, pos);
   }
+
+  dst.setUint32(lenPos, pos - startPos);
+
+  return pos;
+}
+
+export function encodeMessage(a) {
+  if (!Array.isArray(a)) a = [a];
+
+  const len = calculateMessageLength(a);
+  const buf = new ArrayBuffer(len);
+  const dst = new DataView(buf);
+
+  const pos = encodeMessageTo(dst, 0, a, 0, a.length);
 
   if (pos != len) throw new Error('Message length mismatch.');
 
