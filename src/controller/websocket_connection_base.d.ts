9ea4c89a23b5a98276a63db26f6466f9fea3272c
Arne Goedeke <arne@deuso.de>
2023-03-05 12:05:29 +0100

    WebSocketConnection: Add typescript declarations

---
+++ src/controller/websocket_connection_base.d.ts
@@ -0,0 +1,18 @@
+import {
+  IClientConnectionOptions,
+  ClientConnection,
+} from './client_connection';
+
+export interface IWebSocketConnectionBaseOptions
+  extends IClientConnectionOptions {
+  url: string;
+}
+
+/**
+ * :class:`ClientConnection` subclass which implements OCP.1 with WebSocket
+ * transport. This is a generic base class used both by NodeJS and in the
+ * Browser.
+ */
+export class WebSocketConnectionBase extends ClientConnection {
+  write(buf: ArrayBuffer): void;
+}
---
+++ src/controller/websocket_connection_node.d.ts
@@ -0,0 +1,18 @@
+import {
+  WebSocketConnectionBase,
+  IWebSocketConnectionBaseOptions,
+} from './websocket_connection_base';
+import WebSocket from 'ws';
+
+export type WebSocketConnectionOptions = IWebSocketConnectionBaseOptions;
+
+export class WebSocketConnection extends WebSocketConnectionBase {
+  constructor(ws: WebSocket, options: WebSocketConnectionOptions);
+  static connectWebSocket(
+    WebSocket: typeof WebSocket,
+    options: WebSocketConnectionOptions
+  ): Promise<WebSocket>;
+  static connect(
+    options: WebSocketConnectionOptions
+  ): Promise<WebSocketConnection>;
+}
