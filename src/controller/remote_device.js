5636c83ce5b658fab0add357fe4b40b608ce6a4c
Arne Goedeke <arne@deuso.de>
2023-10-16 10:44:39 +0200

    RemoteDevice: Fix role name collision handling
    When creating a rolemap for a device in which two objects have
    the same role name and also an objects exists which has that role name
    with a numeral suffix the tree_to_rolemap function would end in an
    infinite loop.
    
    This commit also adds some tests to check that the handling of that
    situation works correctly.

--- src/controller/remote_device.js
+++ src/controller/remote_device.js
@@ -16,6 +16,7 @@ import { OcaTaskManager } from './ControlClasses/OcaTaskManager.js';
 import { OcaCodingManager } from './ControlClasses/OcaCodingManager.js';
 import { OcaDiagnosticManager } from './ControlClasses/OcaDiagnosticManager.js';
 import { OcaBlock } from './ControlClasses/OcaBlock.js';
+import tree_to_rolemap from './tree_to_rolemap.js';
 
 import * as RemoteControlClasses from './ControlClasses.js';
 
@@ -27,82 +28,6 @@ function eventToKey(event) {
   const id = event.EventID;
 
   return [ono, id.DefLevel, id.EventIndex].join(',');
-}
-
-function tree_to_rolemap(tree, s) {
-  const roles = new Map();
-  if (!s) s = '/';
-
-  const tasks = [];
-
-  const fetch_role = (o) => {
-    if (Array.isArray(o)) {
-      o.forEach(fetch_role);
-    } else {
-      tasks.push(
-        o.GetRole().then((role) => {
-          roles.set(o, role);
-        })
-      );
-    }
-  };
-
-  tree.forEach(fetch_role);
-
-  return Promise.all(tasks).then(function () {
-    const rolemap = new Map();
-
-    const build_paths = (a, prefix) => {
-      const p = prefix != null ? prefix + s : '';
-      const local_roles = new Map();
-
-      a.forEach(function (o, i) {
-        if (Array.isArray(o)) return;
-
-        const role = roles.get(o);
-
-        if (local_roles.has(role)) {
-          const tmp = local_roles.get(role);
-
-          if (Array.isArray(tmp)) tmp.push(o);
-          else local_roles.set(role, [tmp, o]);
-        } else {
-          local_roles.set(role, o);
-        }
-      });
-
-      local_roles.forEach(function (o, role) {
-        if (Array.isArray(o)) {
-          let n = 1;
-          for (let i = 0; i < o.length; i++) {
-            const nrole = role + n;
-            while (local_roles.has(nrole)) {
-              n++;
-            }
-            local_roles.set(nrole, o[i]);
-            local_roles.set(o[i], nrole);
-          }
-
-          local_roles.delete(role);
-        } else {
-          local_roles.set(o, role);
-        }
-      });
-
-      a.forEach((o, i) => {
-        if (Array.isArray(o)) {
-          build_paths(o, p + local_roles.get(a[i - 1]));
-        } else {
-          const path = p + local_roles.get(o);
-          rolemap.set(path, o);
-        }
-      });
-    };
-
-    build_paths(tree, null);
-
-    return rolemap;
-  });
 }
 
 const subscriberMethod = {
---
+++ tests/tree_from_rolemap.test.js
@@ -0,0 +1,71 @@
+import tree_to_rolemap from '../src/controller/tree_to_rolemap.js';
+import { describe, it } from 'node:test';
+import { deepEqual } from 'node:assert';
+
+function createMock(roleName) {
+  return {
+    async GetRole() { return roleName; }
+  };
+}
+
+describe('tree_to_rolemap', () => {
+  it('empty tree', async () => {
+    deepEqual(
+      await tree_to_rolemap([]),
+      new Map()
+    );
+  });
+
+  it('basic', async () => {
+    const foo = createMock('foo');
+    const bar = createMock('bar');
+    deepEqual(
+      await tree_to_rolemap([ foo, bar ]),
+      new Map([
+        [ 'foo', foo ],
+        [ 'bar', bar ]
+      ])
+    );
+  });
+
+  it('with depth', async () => {
+    const foo = createMock('foo');
+    const bar = createMock('bar');
+    const flu = createMock('flu');
+    deepEqual(
+      await tree_to_rolemap([ foo, [ bar, flu ] ]),
+      new Map([
+        [ 'foo', foo ],
+        [ 'foo/bar', bar ],
+        [ 'foo/flu', flu ]
+      ])
+    );
+  });
+
+  it('duplicate roles 1', async () => {
+    const foo1 = createMock('foo');
+    const foo2 = createMock('foo');
+    deepEqual(
+      await tree_to_rolemap([ foo1, foo2 ]),
+      new Map([
+        [ 'foo1', foo1 ],
+        [ 'foo2', foo2 ]
+      ])
+    );
+  });
+
+  it('duplicate roles 2', async () => {
+    const foo1 = createMock('foo1');
+    const foo_1 = createMock('foo');
+    const foo_2 = createMock('foo');
+    deepEqual(
+      await tree_to_rolemap([ foo1, foo_1, foo_2 ]),
+      new Map([
+        [ 'foo1', foo1 ],
+        [ 'foo2', foo_1 ],
+        [ 'foo3', foo_2 ]
+      ])
+    );
+  });
+  
+});
