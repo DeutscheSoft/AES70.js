dc2001dfd8822e181518ad787bb845982ec57c06
Arne Goedeke <arne@deuso.de>
2025-03-03 13:45:09 +0100

    EncodedArguments: Encode data early
    Encoding the arguments early produces better errors when
    passing the wrong data to a remote method. The disadvantage is slightly
    more allocation cost.

--- src/OCP1/encoded_arguments.js
+++ src/OCP1/encoded_arguments.js
@@ -1,37 +1,49 @@
-export class EncodedArguments {
-  get byteLength() {
-    let byteLength = this._byteLength;
+function calculateByteLength(encoders, data) {
+  let byteLength = 0;
 
-    if (byteLength === -1) {
-      const encoders = this.encoders;
-      const data = this.data;
-
-      byteLength = 0;
-
-      for (let i = 0; i < encoders.length; i++) {
-        byteLength += encoders[i].encodedLength(data[i]);
-      }
-      this._byteLength = byteLength;
-    }
-
-    return byteLength;
+  for (let i = 0; i < encoders.length; i++) {
+    byteLength += encoders[i].encodedLength(data[i]);
   }
 
+  return byteLength;
+}
+
+function encode(encoders, data, byteLength) {
+  if (!byteLength) return null;
+  const result = new ArrayBuffer(byteLength);
+  const dataView = new DataView(result);
+
+  for (let i = 0, pos = 0; i < encoders.length; i++) {
+    pos = encoders[i].encodeTo(dataView, pos, data[i]);
+  }
+
+  return result;
+}
+
+export class EncodedArguments {
   constructor(encoders, data) {
     this.encoders = encoders;
     this.data = data;
-    this._byteLength = -1;
+    this.byteLength = calculateByteLength(encoders, data);
+    this.buffer = encode(encoders, data, this.byteLength);
   }
 
   encodeTo(dataView, pos) {
     pos = pos | 0;
 
-    const { encoders, data } = this;
+    const { byteLength, buffer } = this;
 
-    for (let i = 0; i < encoders.length; i++) {
-      pos = encoders[i].encodeTo(dataView, pos, data[i]);
-    }
+    if (!byteLength) return pos;
 
-    return pos;
+    const src = new Uint8Array(buffer);
+    const dst = new Uint8Array(
+      dataView.buffer,
+      dataView.byteOffset,
+      dataView.byteLength
+    );
+
+    dst.set(src, pos);
+
+    return pos + byteLength;
   }
 }
