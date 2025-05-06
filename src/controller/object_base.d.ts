4e819fa0982a7b3e22ac2e843e00d36b05337b9c
Arne Goedeke <arne@deuso.de>
2025-05-06 10:57:44 +0200

    Add typescript declarations for ObjectBase

---
+++ src/controller/object_base.d.ts
@@ -0,0 +1,34 @@
+import { RemoteDevice } from './remote_device';
+import { IOcaPropertyID, OcaPropertyID } from '../types/OcaPropertyID';
+
+export class ObjectBase {
+  readonly device: RemoteDevice;
+  readonly ono: number;
+
+  constructor(ObjectNumber: number, device: RemoteDevice);
+
+  /**
+   * Returns the object number.
+   */
+  get ObjectNumber(): number;
+
+  /**
+   * Returns the class version of this implementation.
+   */
+  get ClassVersion(): number;
+
+  /**
+   * Returns the class id of this implementation.
+   */
+  get ClassID(): string;
+
+  /**
+   * Returns the name of this class.
+   */
+  get ClassName(): string;
+
+  GetPropertyName(id: IOcaPropertyID): string | undefined;
+  GetPropertyID(name: string): OcaPropertyID;
+
+  Dispose(): void;
+}
